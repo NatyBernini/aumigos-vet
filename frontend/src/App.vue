388ec4c384a87335ff3894bc0b5ea37963c90956
Natália <primeiroperiodonaty@gmail.com>
2025-07-07 21:27:03 -0300

    feat: estilização tabela

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,3 +5,20 @@
 <script setup>
 import MainLayout from './layouts/MainLayout.vue'
 </script>
+
+<style lang="scss">
+.title-pag {
+  font-size: 26px;
+  color: #434343;
+  font-weight: 600!important;
+}
+
+.breadcrumb {
+  color: #535353;
+  font-size: 15px;
+}
+
+.page-active {
+  color: #d31b27d7;
+}
+</style>
